a40450c4713fad9c7a2d45f1322cdb88338ea8ec
aisultankassenov <aisultan.kassenov@nu.edu.kz>
2019-10-18 12:37:17 +0600

    feat(backend): initial commit

--- back-end/src/main/java/user/model/User.java
+++ back-end/src/main/java/user/model/User.java
@@ -1,4 +1,12 @@
 package user.model;
 
 public class User {
+
+    private String login;
+    private String password;
+
+    public User(String login, String password){
+        this.login = login;
+        this.login = password
+    }
 }
